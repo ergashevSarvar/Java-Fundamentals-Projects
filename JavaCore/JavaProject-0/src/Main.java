ce077b1356c5e856bdf6bc07c73103d646cedcf9
anonymous7se <sarvarergashev776@gmail.com>
2022-06-04 18:58:33 +0500

    Project initalization

---
+++ JavaCore/JavaProject-0/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    String name = "Sarvar";
+    int age = 24;
+    String gender = "M";
+    boolean isHappy = true;
+    System.out.println("Hello my name is: " + name + " " + "my age " + age);
+  }
+}
