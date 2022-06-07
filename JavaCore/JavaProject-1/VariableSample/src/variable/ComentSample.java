eff3f8560915b6a37e9c701b3221b6deac930c24
anonymous7se <sarvarergashev776@gmail.com>
2022-06-07 21:00:13 +0500

    Comment types and Variables

---
+++ JavaCore/JavaProject-1/VariableSample/src/variable/ComentSample.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class ComentSample {
+  public static void main(String[] args) {
+    // step-0. Comment
+    //    int a = 10; simple comment
+    /*
+    *
+    * Java
+    * Document
+    * Aksaleks
+    * @information comment
+    * universal comment
+    * */
+
+    /* int b = 2; */
+    int a = 11;
+  }
+}
---
+++ JavaCore/JavaProject-1/VariableSample/src/variable/Variables.java
@@ -0,0 +1,33 @@
+package variable;
+
+public class Variables {
+  public static void main(String[] args) {
+    // step-0. primitive type
+
+    int number = 100;
+    double sum = 123.3;
+    char letter = 'a';
+    boolean tellMe = true;
+    String name = "Sarvar";
+    float size = 123.2f;
+
+    // step-1. Long type.
+    long bigSize = 100L;
+    System.out.println(bigSize + sum);
+
+    // step-2. Math exercise.
+
+    int num2 = number + 100;
+    System.out.println("Sum: " + num2);
+
+  }
+}
+
+// TASK
+
+/*
+* Type
+* Comment
+* Math
+* */
+
